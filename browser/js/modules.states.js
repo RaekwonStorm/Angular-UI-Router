f99c76dcef5d21bdb0a4c0dd187aff8576a313a4
Reed Branson <reed.branson@gmail.com>
2016-07-07 16:15:53 -0400

    refactored sidebar albums view

---
+++ browser/js/modules.states.js
@@ -0,0 +1,11 @@
+'use strict';
+
+juke.config(function ($stateProvider) {
+
+  $stateProvider.state("albumList", {
+    url: '/albums',
+    templateUrl: '../views/albums.html',
+    controller: 'AlbumsCtrl'
+  });
+
+});
